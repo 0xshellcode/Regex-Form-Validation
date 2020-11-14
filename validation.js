9c31e53262c959f09c9d4129a99c52ce22265904
0xshellcode <38792094+0xshellcode@users.noreply.github.com>
2020-11-14 09:54:45 -0600

    Add files via upload

---
+++ validation.js
@@ -0,0 +1,30 @@
+/*
+var reg = /[a-z]/ig;
+var reg2 = new RegExp(/[a-z]/, 'ig');
+*/
+
+const inputs = document.querySelectorAll('input');
+
+const patterns = {
+    username: /^[a-z\d]{5,12}$/i,
+    email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
+    password: /^[\w@-]{8,20}$/, 
+    telephone: /^\d{11}$/,
+    slug: /^[a-z\d-]{8,20}$/
+};
+
+// Validation function
+
+function validation(field, regex){
+    if(regex.test(field.value)){
+        field.className = 'valid';
+    } else {
+        field.className = 'invalid';
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener('keyup', (e) =>{
+        validation(e.target, patterns[e.target.name])
+    });
+})
